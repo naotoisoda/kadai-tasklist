4a2c44f4cb3400e66bbbaee89275b68bcf122b11
naoto.isoda <naoto.isoda@offisis.co.jp>
2020-12-04 07:06:23 +0000

    migration_error

--- database/migrations/2020_12_03_180656_add_person_to_tasklist.php
+++ database/migrations/2020_12_03_180656_add_person_to_tasklist.php
@@ -14,7 +14,7 @@ class AddPersonToTasklist extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('person');
+            
         });
     }
 
